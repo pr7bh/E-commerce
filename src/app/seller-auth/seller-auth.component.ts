58f94d5eeb79216a46737dd75f4b95935fe0114b
Prabhjot <prabhjotsinghrainu@gmail.com>
2025-02-02 22:36:31 +0530

    seller auth added

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
   templateUrl: './seller-auth.component.html',
   styleUrl: './seller-auth.component.css'
 })
-export class SellerAuthComponent {
+export class SellerAuthComponent implements OnInit {
+  constructor() {}
 
+  ngOnInit(): void {}
+
+  signUp(data: object): void {
+    console.warn(data);
+  }
 }
